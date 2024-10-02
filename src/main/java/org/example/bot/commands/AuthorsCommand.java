97bbf81bc737bff5c72ed4c3c160e5ffa95dd11b
sobol <egorsobolsobol@yandex.ru>
2024-10-03 00:49:57 +0500

    Полная реализация всех команд

--- src/main/java/org/example/bot/commands/AuthorsCommand.java
+++ src/main/java/org/example/bot/commands/AuthorsCommand.java
@@ -2,13 +2,19 @@ package org.example.bot.commands;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
 public class AuthorsCommand implements Command {
-    @Override
-    public SendMessage execute(Update update) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(update.getMessage().getChatId().toString());
-        sendMessage.setText("@ZAntoshkAZ @polska_stronker @sobol_eg");
-        return sendMessage;
+    public String getDescription (){
+        return "Авторы";
     }
+
+    @Override
+    public String getContent() {
+        return "@ZAntoshkAZ @polska_stronker @sobol_eg";
+    }
+
+    @Override
+    public String getCommand() {
+        return "/authors";
+    }
+
 }
--- src/main/java/org/example/bot/commands/InfoCommand.java
+++ src/main/java/org/example/bot/commands/InfoCommand.java
@@ -4,11 +4,19 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class InfoCommand implements Command {
+
     @Override
-    public SendMessage execute(Update update) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(update.getMessage().getChatId().toString());
-        sendMessage.setText("Это универсалльный бот, который поможет вам выбрать блюдо, написать рецепт из имеющихся у сам ингредиентов и многое другое");
-        return sendMessage;
+    public String getDescription() {
+        return "Информация о боте";
+    }
+
+    @Override
+    public String getContent() {
+        return "Это универсальный бот, который поможет вам выбрать блюдо, написать рецепт из имеющихся у вас ингредиентов и многое другое";
+    }
+
+    @Override
+    public String getCommand() {
+        return "/info";
     }
 }
